b8056ac30b959278fcbda83e7ddd172ba29e23ee
balicz3k <baliczek_10@vp.pl>
2025-09-30 14:22:26 +0200

    2221. Find Triangular Sum of an Array.

---
+++ cpp/2221_FindTriangularSumOfAnArray.cpp
@@ -0,0 +1,21 @@
+#include #include < bits / stdc++.h>
+
+class Solution
+{
+public:
+    int triangularSum(std::vector<int>& nums)
+    {
+        int n = nums.size() - 1;
+        for (int i{0}; i < n; i++)
+        {
+            nums[i] += nums[i + 1];
+            nums[i] %= 10;
+            if (i == n - 1)
+            {
+                --n;
+                i = -1;
+            }
+        }
+        return nums[0];
+    }
+};
